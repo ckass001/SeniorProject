debad05eadd14baab73f42abf7e484608d97130f
ckass001 <ckass001@odu.edu>
2021-10-26 20:54:58 -0400

    Coffee Mechanics
    Added ability to sell coffee to "people" and track a balance for it.

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -5,15 +5,23 @@ using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int orangeCounter = 0;
-    [SerializeField] private Text orangeCounterText;
+    private int coffeeCounter = 0;
+    public int balance;
+    public int costOfCoffee = 5;
+    [SerializeField] private Text coffeeCounterText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Coffee"))
         {
             Destroy(collision.gameObject);
-            orangeCounter++;
-            orangeCounterText.text = "Coffee Counter: " + orangeCounter;
+            coffeeCounter++;
+            coffeeCounterText.text = "Coffee Counter: " + coffeeCounter + System.Environment.NewLine + "Balance: " + balance;
+        }
+        if(collision.gameObject.CompareTag("Buyer"))
+        {
+            balance += coffeeCounter * costOfCoffee;
+            coffeeCounter = 0;
+            coffeeCounterText.text = "Coffee Counter: " + coffeeCounter + System.Environment.NewLine + "Balance: " + balance;
         }
     }
 }
