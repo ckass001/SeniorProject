9ff5bb75fb57db721a4bd9a5a9d56dd90c4323cb
sanyele <86446927+sanyele@users.noreply.github.com>
2021-11-07 17:47:50 -0500

    Updated delay

--- Assets/Scripts/Delay.cs
+++ Assets/Scripts/Delay.cs
@@ -10,17 +10,25 @@ public class Delay : MonoBehaviour
     {
         DOVirtual.DelayedCall(3, GotoNextScene);
 
-        DOVirtual.DelayedCall(3, GotoNextScene2);
+      //  DOVirtual.DelayedCall(3, GotoNextScene2);
     }
 
     // Update is called once per frame
+    
+  /* void Update()
+    {
+    
+        DOVirtual.DelayedCall(3, GotoNextScene2);
+    } */
+
+    void GotoNextScene2 ()
+    {
+        SceneManager.LoadScene("Lesson UI");
+    }
+
     void GotoNextScene ()
     {
         SceneManager.LoadScene("Apartment1");
     }
 
-     void GotoNextScene2 ()
-    {
-        SceneManager.LoadScene("Lesson UI");
-    }
 }
