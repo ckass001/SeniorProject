5313df45989e9a624ca99d90103936a3f8dab64a
sanyele <86446927+sanyele@users.noreply.github.com>
2021-11-21 18:23:04 -0500

    Finished Mid and High Apartments

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -10,6 +10,16 @@ public class SceneChanger : MonoBehaviour
         SceneManager.LoadScene("Apartment1");
     }
 
+    public void Apartment2 ()
+    { 
+        SceneManager.LoadScene("Apartment2");
+    }
+
+    public void Apartment3 ()
+    { 
+        SceneManager.LoadScene("Apartment3");
+    }
+
      public void Load()
     {
         SceneManager.LoadScene("LoadingScreen");
